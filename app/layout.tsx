6d031fbe5268beb18f21aa21b386c608df7536b5
Marozva2 <isabokefrankline@gmail.com>
2024-12-24 10:44:22 +0300

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import BackToTopButton from "./components/BackToTopButton";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main>{children}</main>
+        <main>
+          {children}
+          <Analytics />
+        </main>
         <BackToTopButton />
       </body>
     </html>

b2b5f07a1b54e21a27188f259ece5d80bfee3ae0
Marozva2 <isabokefrankline@gmail.com>
2025-01-14 08:57:55 +0300

    added theme dependency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
 import type { Config } from "tailwindcss";
+// import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lightBackground: "#F7F7F7", // Light mode background
+        darkBackground: "#1A1A1A", // Dark mode background
+        lightForeground: "#333333", // Light mode text
+        darkForeground: "#FFFFFF", // Dark mode text
+      },
+      transitionProperty: {
+        theme: "background-color, color",
       },
     },
   },
-  plugins: [],
+  // plugins: [tailwindcssAnimate],
 } satisfies Config;
